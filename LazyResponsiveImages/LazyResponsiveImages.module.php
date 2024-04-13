ddf1a28d0dcda74e7776ad95b45a8ea5433d676e
Paul Ashby <paul@primitive.co>
2024-04-13 14:25:40 +0100

    Output animated gifs using Pageimages array for size variations

--- LazyResponsiveImages/LazyResponsiveImages.module.php
+++ LazyResponsiveImages/LazyResponsiveImages.module.php
@@ -20,7 +20,44 @@ class LazyResponsiveImages extends WireData implements Module {
     }
     public function init() {
         $this->addHookAfter('InputfieldFile::fileAdded', $this, 'sizeImage');
+        $this->wire->addHook('Pageimages::hasOnly', $this, "hasOnlyExtension");
     }
+
+    /**
+     * Check that all images in PageImages array have the given extension
+     *
+     * @param  HookEvent $event
+     *
+     * @return Boolean $result
+     */
+    public function hasOnlyExtension($event) {
+
+        $pageimages = $event->object;
+        $extension = $event->arguments(0);
+
+        // Check that all images have the given extension
+        $result = $this->imageExtensionsMatch($pageimages, [$extension]);
+        $event->return = $result;
+    }
+
+    /**
+     * Iterate PageImages array to check file extensions match given value
+     *
+     * @param  PageImages $pageimages
+     * @param  Array $extensions
+     *
+     * @return Boolean
+     */
+    private function imageExtensionsMatch($pageimages, $extensions) {
+
+        foreach($pageimages as $image) {
+            if (!in_array($image->ext, $extensions)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     /**
      * Store info for created elements and pass to completeInstall function
      *
@@ -68,8 +105,8 @@ class LazyResponsiveImages extends WireData implements Module {
 
     }
     /*
-           Convert configuration entry to array
-       */
+        Convert configuration entry to array
+    */
     public function configStringToArray($str) {
 
         parse_str($str, $arr);
@@ -118,11 +155,11 @@ class LazyResponsiveImages extends WireData implements Module {
 
     public function renderImage($options) {
         $image = $options["image"];
+        $art_directed = is_array($image);
         $webp = $options["webp"] ?? false;
         $alt_str = str_replace("'", "", $options["alt_str"]); // Remove apostrophes
         $options["img_class"] = $options["class"] ?? "";
         $variations = [];
-        $art_directed = is_array($options["image"]);
         $has_source = $webp || $art_directed;
         $lazy_load = $options["lazy_load"] ?? false;
         $data_prfx = "";
@@ -151,24 +188,23 @@ class LazyResponsiveImages extends WireData implements Module {
                         "media" => $variant["media"],
                         "sizes" => $variant["sizes"],
                         "data_prfx" => $data_prfx,
-                        "variations" => $variations
+                        "variations" => $variations,
+                        "is_animated_gif" => $this->isAnimatedGif($variant["image"], $webp)
                     ];
-
                     $source_markup .= $this->getSourceElmts($source_options, $webp, $art_directed);
                 }
             } else {
                 // get source for the single image with webp version
                 $variations = $this["image_spec"][$options["field_name"]];
                 $options["variations"] = $variations;
-
                 $source_options = [
                     "image" => $image,
                     "media" => false,
                     "sizes" => $options["sizes"],
                     "data_prfx" => $data_prfx,
-                    "variations" => $variations
+                    "variations" => $variations,
+                    "is_animated_gif" => $this->isAnimatedGif($image, $webp)
                 ];
-
                 $source_markup = $this->getSourceElmts($source_options, $webp, $art_directed);
             }
 
@@ -188,6 +224,7 @@ class LazyResponsiveImages extends WireData implements Module {
         }
 
         // Not art directed or webp - get standalone image markup
+        $options["is_animated_gif"] = $this->isAnimatedGif($options["image"], $webp);
         $variations = $this["image_spec"][$options["field_name"]];
         $options["variations"] = $variations;
         $srcset = $this->getSrcset($options, $webp);
@@ -202,11 +239,40 @@ class LazyResponsiveImages extends WireData implements Module {
                 </noscript>";
     }
 
-    private function getSrcUrl($url_options, $art_directed = false) {
+    private function isAnimatedGif($image, $webp) {
+        if(!$this->exclude_gifs || get_class($image) !== "ProcessWire\Pageimages") {
+            // We're outputting static gifs
+            // Or
+            // we haven't been passed a Pageimages array, so we don't have pre-sized variation images
+            return false;
+        }
 
+        /*
+        * $image is a Pageimage array.
+        * We only expect this for animated gifs, in which
+        * case each image is a pre-sized variation.
+        * This is necessary as ProcessWire's image sizer is
+        * discarding all but the first frame of animated gifs.
+        *
+        */
+        if ($image->hasOnly("gif")) {
+            if ($webp) {
+                throw new WireException("Pageimage array of gifs will output animated_gifs, but images are specified as webp.");
+            }
+            return true;
+        } else {
+            throw new WireException("Expected image options array, got ProcessWire PageImages array.");
+        }
+    }
+
+    private function getSrcUrl($url_options, $art_directed = false) {
         $image = $art_directed ? end($url_options["image"])["image"] : $url_options["image"];
         $context = $url_options["context"] = $url_options["context"] ?? "";
         $fallbacks = $this["image_fallback_spec"] ?? false;
+
+        if (array_key_exists("is_animated_gif", $url_options) && $url_options["is_animated_gif"]) {
+            return $image->first()->url;
+        }
 
         if ($fallbacks && array_key_exists($context, $fallbacks) && strlen($fallbacks[$context])) {
             return $image->size($fallbacks[$context], 0)->url;
@@ -219,6 +285,7 @@ class LazyResponsiveImages extends WireData implements Module {
         $media_str = !$art_directed ? "" : "media='{$source_options["media"]}'";
         $sizes = $source_options["sizes"];
         $source_elmts = "";
+
         // Source for webp
         if ($webp) {
             $webp_srcset = $this->getSrcset($source_options, $webp);
@@ -235,14 +302,24 @@ class LazyResponsiveImages extends WireData implements Module {
     public function getSrcset($srcset_options, $webp) {
         $srcset = "";
 
-        foreach ($srcset_options["variations"] as $size) {
-            $var_img = $srcset_options["image"]->size($size, 0);
-            if ($webp) {
-                $srcset .= $var_img->webp->url . " {$size}w,";
-            } else {
-                $srcset .= $var_img->url . " {$size}w,";
+        if ($srcset_options["is_animated_gif"]) {
+            // Output animated gifs at natural sizes
+            $srcset_options["image"]->sort("width");
+            foreach ($srcset_options["image"] as $var_img) {
+                $url = $var_img->url;
+                $width = $var_img->width;
+                $srcset .= "{$url} {$width}w, ";
+            }
+        } else {
+            foreach ($srcset_options["variations"] as $size) {
+                $var_img = $srcset_options["image"]->size($size, 0);
+                if ($webp) {
+                    $srcset .= $var_img->webp->url . " {$size}w, ";
+                } else {
+                    $srcset .= $var_img->url . " {$size}w, ";
+                }
             }
         }
-        return substr($srcset, 0, -1); // Remove trailing comma
+        return substr($srcset, 0, -2); // Remove trailing comma and space
     }
 }
